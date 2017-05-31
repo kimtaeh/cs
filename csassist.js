6c849d81e932eccb776f9b4bd0224a9a28e4ab95
kimtaeh <qed0044@gmail.com>
2017-05-31 11:10:22 +0900

    Update csassist.js

--- csassist.js
+++ csassist.js
@@ -64,18 +64,6 @@ app.post('/',function(request,response){
 			cs_intent = '';
 			cs_message = '고객님이 주문하신 '+ cs_order + '번 주문은 담당기사님께서 배송 중에 있습니다. \n\n 배송의 경우 상품 발송 후 수령까지 약 1~2일 정도 시간이 소요됩니다.'
 		}
-		elseif (cs_order == 1){
-			cs_intent = 'delCheck'; 
-			cs_message = '고객님이 최근 주문하신 물건 중 배송 조회가 가능한 주문은 아래와 같습니다. \n\n 2017-05-31 주문번호 1101번 나이키 운동화, \n\n 2017-05-29 1008번 신라면 \n\n 어떤 주문을 조회하고 싶으신가요?'
-		}
-		elseif (cs_order == 2){
-			cs_intent = 'delReturn'; 
-			cs_message = '고객님이 최근 주문하신 물건 중 배송 조회가 가능한 주문은 아래와 같습니다. \n\n 2017-05-31 주문번호 1101번 나이키 운동화, \n\n 2017-05-29 1008번 신라면 \n\n 어떤 주문을 조회하고 싶으신가요?'
-		}
-		elseif (cs_order < 10){
-			cs_intent = ''; 
-			cs_message = '지원되지 않는 메뉴입니다. \n\n 정확한 메뉴를 선택해주세요 \n\n (1번 배송, 2번 반품)';
-		}
 		else{
 			cs_intent = cs_type;
 			cs_message = '입력하신 '+ cs_order + '번 주문은 조회가 가능하지 않습니다. \n\n  주문번호를 다시 확인해주세요' 
@@ -96,14 +84,6 @@ app.post('/',function(request,response){
 		elseif (cs_order == 1008){
 			cs_intent = '';
 			cs_message = '주문의 경우 이미 배송이 시작되어 취소 신청이 되지 않습니다. \n\n 요청주신 취소 건은 상담원에게 전달 후 처리를 지원해드리겠습니다.'
-		}
-		elseif (cs_order == 1){
-			cs_intent = 'delCheck'; 
-			cs_message = '고객님이 최근 주문하신 물건 중 배송 조회가 가능한 주문은 아래와 같습니다. \n\n 2017-05-31 주문번호 1101번 나이키 운동화, \n\n 2017-05-29 1008번 신라면 \n\n 어떤 주문을 조회하고 싶으신가요?'
-		}
-		elseif (cs_order == 2){
-			cs_intent = 'delReturn'; 
-			cs_message = '고객님이 최근 주문하신 물건 중 배송 조회가 가능한 주문은 아래와 같습니다. \n\n 2017-05-31 주문번호 1101번 나이키 운동화, \n\n 2017-05-29 1008번 신라면 \n\n 어떤 주문을 조회하고 싶으신가요?'
 		}
 		else{
 			cs_intent = cs_type;
