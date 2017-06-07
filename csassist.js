26a5c460c8e084785fa9441e951084a012d84bb8
kimtaeh <qed0044@gmail.com>
2017-06-07 09:34:58 +0900

    Update csassist.js

--- csassist.js
+++ csassist.js
@@ -15,6 +15,56 @@ var cs_input_cnt = 0;
 //reply define
 
 
+//rest_api : keyboard 
+app.post('/keyboard',function(request,response){
+ 
+    console.log(request.body);
+
+    cs_message = "무엇을 도와드릴까요";
+    response.json({
+    "message" : {
+          "text" : cs_message 
+        }
+    });
+
+});
+
+//rest_api : message
+app.post('/message',function(request,response){
+ 
+    console.log(request.body);
+
+    cs_message = "할룽";
+    response.json({
+    "message" : {
+          "text" : cs_message 
+        }
+    });
+
+});
+
+var server = http.createServer(app).listen(80, function () {
+console.log('server running...')
+});
+
+/*
+//module call
+var express = require('express');
+var http = require('http');
+var bodyParser = require('body-parser');
+var app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+
+//data predefine
+var cs_intent = '';
+var cs_message_log = new Array();
+var cs_input_cnt = 0;
+
+//reply define
+
+
 //rest_api
 app.post('/',function(request,response){
  
@@ -427,3 +477,4 @@ app.post('/',function(request,response){
 var server = http.createServer(app).listen(80, function () {
 console.log('server running...')
 });
+*/
