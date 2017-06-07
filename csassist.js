c0ce4849a2c20f39a9deef8b0dac41173a1fe7e5
kimtaeh <qed0044@gmail.com>
2017-06-07 10:16:12 +0900

    Update csassist.js

--- csassist.js
+++ csassist.js
@@ -21,6 +21,7 @@ app.get('/keyboard',function(request,response){
     console.log(request.body);
 
     cs_message = "무엇을 도와드릴까요";
+    response.setHeader('Content-Type', 'application/json; charset=utf-8');
     response.json({
     "message" : {
           "text" : cs_message 
@@ -35,6 +36,7 @@ app.post('/message',function(request,response){
     console.log(request.body);
 
     cs_message = "할룽";
+    response.setHeader('Content-Type', 'application/json; charset=utf-8');
     response.json({
     "message" : {
           "text" : cs_message 
