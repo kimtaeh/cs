5e9327b8434e7b2860a4aaa5a2786750e8765afb
kimtaeh <qed0044@gmail.com>
2017-06-21 12:52:16 +0900

    Update csassist.js

--- csassist.js
+++ csassist.js
@@ -200,11 +200,16 @@ app.post('/',function(request,response){
 				      "template_type": "generic",
 				      "elements": [
 								{
-								  "title": "운동화·슈즈",
+								  "title": "",
 								  "subtitle": "17903개의 상품",
 								  "item_url": "http://m.g9.co.kr/Category.htm#/Display/G9Category/400000080",
 								  "image_url": "http://image.g9.co.kr/i/2016/10/19/b3b0b6dc176a45f4.png",
 								  "buttons": [
+								    {
+								      "type": "postback",
+								      "title": "보기",
+								      "payload": "SEARCH_IN_CATE 400000080 나이키"
+								    },
 								    {
 								      "type": "postback",
 								      "title": "보기",
