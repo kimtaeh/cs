7fc410b2742ecd82d5dc7a4fd762b7e0f22aa82d
kimtaeh <qed0044@gmail.com>
2017-06-21 09:41:53 +0900

    Update csassist.js

--- csassist.js
+++ csassist.js
@@ -94,24 +94,13 @@ app.post('/',function(request,response){
 					        "elements":[
 					           {
 					            "title":"Welcome to Peter\'s Hats",
-					            "image_url":"https://petersfancybrownhats.com/company_image.png",
-					            "subtitle":"We\'ve got the right hat for everyone.",
-					            "default_action": {
-					              "type": "web_url",
-					              "url": "https://peterssendreceiveapp.ngrok.io/view?item=103",
-					              "messenger_extensions": true,
-					              "webview_height_ratio": "tall",
-					              "fallback_url": "https://peterssendreceiveapp.ngrok.io/"
-					            },
 					            "buttons":[
 					              {
-					                "type":"web_url",
-					                "url":"https://petersfancybrownhats.com",
-					                "title":"View Website"
+					                "type":"postback",
+					                "title":"yes",
 					              },{
 					                "type":"postback",
-					                "title":"Start Chatting",
-					                "payload":"DEVELOPER_DEFINED_PAYLOAD"
+					                "title":"no",
 					              }              
 					            ]      
 					          }
