59d9d5ce05b0a5f5f1e4fe866a4003170509d6ff
kimtaeh <qed0044@gmail.com>
2017-06-26 01:11:51 +0900

    Update test.js

--- test.js
+++ test.js
@@ -1,16 +1,42 @@
 var mecab = require('mecab-ya');
 
+var faq_list = 
+[
+['판매자 통화', '판매자의 주소와 연락처는 상품 상세정보 페이지에서 확인하실 수 있습니다.', 'http://member2.gmarket.co.kr/CustomerCenter/Main']
+['주문 번호 확인','구매한 상품의 주문번호는 나의 쇼핑정보 > 전체주문내역에서 확인 가능합니다','http://member2.gmarket.co.kr//CustomerCenter/FaqList?SearchClass_0=01&SearchClass_1=102&SearchClass_2=10201&SearchClass_3=1020101']
+]
+
 var text = '주문번호는 어디서 확인 하나요';
 
 
 mecab.nouns(text, function (err, result) {
     
-    var t1 = result[0];
+    var message = '';
+    var ret_message = ''
+    var ret_url = ''
+    var t1 = result.lenth;
+    
+    //faq 조회
+	for (var i = 0; i < t1 ; i++){
+		message = message + result(i);
+	}
     
     console.log(result);
-    /*
-        [ '아버지', '방' ]
-    */
+    console.log(message);
+    
+    
+    //faq 조회
+	for (var j = 0; j < 2 ; j++){
+		
+        if ( faq_list[j][0] == message)
+        {
+            ret_message = faq_list[j][1];
+            ret_url = faq_list[j][2];
+        }
+        console.log(ret_message);
+        console.log(ret_url);
+	}
+    
     
     
 });
