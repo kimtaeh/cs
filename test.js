5e983cf2df1dee80410cb7ab89f01c4abef62383
kimtaeh <qed0044@gmail.com>
2017-06-26 01:04:18 +0900

    Update test.js

--- test.js
+++ test.js
@@ -1,12 +1,13 @@
 var mecab = require('mecab-ya');
 
-var text = '아버지가방에들어가신다';
+var text = '판매자와 통화하고 싶어요';
+
 
 mecab.nouns(text, function (err, result) {
     
     var t1 = result[0];
     
-    console.log(t1);
+    console.log(result);
     /*
         [ '아버지', '방' ]
     */
