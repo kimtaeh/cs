6b6fab2728e139565f1d6165e990313887f44621
kimtaeh <qed0044@gmail.com>
2017-06-26 00:58:19 +0900

    Update test.js

--- test.js
+++ test.js
@@ -3,8 +3,13 @@ var mecab = require('mecab-ya');
 var text = '아버지가방에들어가신다';
 
 mecab.nouns(text, function (err, result) {
-    console.log(result);
+    
+    var t1 = result[0];
+    
+    console.log(t1);
     /*
         [ '아버지', '방' ]
     */
+    
+    
 });
