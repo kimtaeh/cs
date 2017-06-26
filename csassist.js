4569a77c334e36978ec81dee9c876e44a92f0627
kimtaeh <qed0044@gmail.com>
2017-06-26 09:03:23 +0900

    Update csassist.js

--- csassist.js
+++ csassist.js
@@ -294,7 +294,7 @@ app.post('/',function(request,response){
 										"template_type": "generic",
 										"elements": [
 											{
-											  "text": "문의 주신 내용은 현재 지원하지 않는 문의 입니다.\n질문 주신 내용에 대해 상담을 원하신다면 아래 버튼을 클릭해주세요",
+											  "title": "문의 주신 내용은 현재 지원하지 않는 문의 입니다.\n질문 주신 내용에 대해 상담을 원하신다면 아래 버튼을 클릭해주세요",
 											  "buttons": [
 											    {
 											      	"type":"web_url",
@@ -533,7 +533,7 @@ app.post('/',function(request,response){
 										"template_type": "generic",
 										"elements": [
 											{
-											  "text": "문의 주신 내용은 현재 지원하지 않는 문의 입니다.\n질문 주신 내용에 대해 상담을 원하신다면 아래 버튼을 클릭해주세요",
+											  "title": "문의 주신 내용은 현재 지원하지 않는 문의 입니다.\n질문 주신 내용에 대해 상담을 원하신다면 아래 버튼을 클릭해주세요",
 											  "buttons": [
 											    {
 											      	"type":"web_url",
@@ -1003,7 +1003,7 @@ app.post('/',function(request,response){
 										"template_type": "generic",
 										"elements": [
 											{
-											  "text": "문의 주신 내용은 현재 지원하지 않는 문의 입니다.\n질문 주신 내용에 대해 상담을 원하신다면 아래 버튼을 클릭해주세요",
+											  "title": "문의 주신 내용은 현재 지원하지 않는 문의 입니다.\n질문 주신 내용에 대해 상담을 원하신다면 아래 버튼을 클릭해주세요",
 											  "buttons": [
 											    {
 											      	"type":"web_url",
