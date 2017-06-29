98a100f6c8a00d50ce40e4bcd0406b792ab9617a
kimtaeh <qed0044@gmail.com>
2017-06-29 10:28:48 +0900

    Update csassist.js

--- csassist.js
+++ csassist.js
@@ -449,7 +449,7 @@ app.post('/',function(request,response){
 										 "payload": {
 											"template_type": "generic",
 											"elements": [
-											{"title": "G마켓에 문의하기로 이동하기", "buttons": [{"type":"web_url","title": "MyG", "url":"http://member2.gmarket.co.kr/CustomerCenter/Main"}]}
+											{"title": "G마켓에 문의하기로 이동하기", "buttons": [{"type":"web_url","title": "", "url":"http://mobile.gmarket.co.kr/CustomerCenter"]}
 											]
 									}
 									}
